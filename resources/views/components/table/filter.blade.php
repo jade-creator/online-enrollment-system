8083b613b2b8bb97be5051ad9cf11cfe2d60d955
jadessoriano <jadelestersoriano@gmail.com>
2021-09-23 01:36:26 +0800

    fix: flashing style on filter modal

--- resources/views/components/table/filter.blade.php
+++ resources/views/components/table/filter.blade.php
@@ -3,11 +3,6 @@
 <div class="flex justify-between items-center mb-4">
     @if ($isSearchable)
         <div class="my-4 w-full md:w-52 relative">
-            <!-- <div class="absolute inset-y-0 left-3 flex items-center pointer-events-none">
-                <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="search" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="w-4">
-                    <path fill="currentColor" d="M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6.1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128 0 70.7-57.2 128-128 128z" class=""></path>
-                </svg>
-            </div> -->
             <input type="text" wire:model.debounce.300ms="search" placeholder="Search...">
         </div>
     @endif
@@ -26,7 +21,8 @@
              x-transition:leave-start="transform opacity-100 scale-100"
              x-transition:leave-end="transform opacity-0 scale-95"
              class="grid place-items-center fixed w-screen h-screen left-0 top-0 z-50 bg-gray-500 bg-opacity-75 overflow-hidden"
-             >
+             style="display: none;"
+             @click="open = false">
             <div class="max-h-96 w-96 py-4 space-y-4 rounded-lg ring-1 ring-black ring-opacity-5 border border-gray-300 bg-white p-4 overflow-y-auto">
                 <h1 class="w-full text-lg ">Filter</h1>
                 @if($isFilterable)
@@ -55,7 +51,7 @@
                 @endif
 
                 {{ $slot }}
-                
+
                 <div class="text-right">
                     <x-jet-secondary-button @click="open = false">
                         {{ __('Close') }}
