871a1298830e06ecac324cb2d64971e4e097dec5
Andrew Ferrer <andrewferrer80@gmail.com>
2021-09-28 19:25:29 +0800

    New loading design

--- resources/views/partials/loading.blade.php
+++ resources/views/partials/loading.blade.php
@@ -1,4 +1,10 @@
-<div wire:loading class="animate-pulse bg-blue-400 relative z-40">
-    <div class="w-full bg-indigo-400 fixed top-0 left-0 h-0.5">
+<div wire:loading class="relative z-50">
+    <div class="grid place-items-center w-full h-full bg-gray-500 bg-opacity-50 fixed top-0 left-0">
+        <div class="flex justify-around w-56 px-3 py-2 text-white bg-indigo-500 rounded-lg shadow-lg">
+            <span>
+                <x-icons.loading-icon color="#FFFFFF" />
+            </span>
+            <p class="text-lg tracking-widest">Loading...</p>
+        </div>
     </div>
 </div>
