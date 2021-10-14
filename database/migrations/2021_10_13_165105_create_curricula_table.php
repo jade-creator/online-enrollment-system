a75220755b84cc5dff0b6fc91682d3573797007e
jadessoriano <jadelestersoriano@gmail.com>
2021-10-14 13:45:00 +0800

    adding curricula

---
+++ database/migrations/2021_10_13_165105_create_curricula_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCurriculaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('curricula', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('program_id')->constrained();
+            $table->string('code', 100);
+            $table->text('description')->nullable();
+            $table->boolean('isActive')->default(0);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('curricula');
+    }
+}
