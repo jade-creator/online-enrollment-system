63ef1990f8bffa0a4b800cd66eff1e23e9eec646
jadessoriano <jadelestersoriano@gmail.com>
2021-11-02 12:24:46 +0800

    optimize query, meta properties open graph, sidebar dropdowns

--- app/Http/Livewire/Admin/Dashboard.php
+++ app/Http/Livewire/Admin/Dashboard.php
@@ -3,16 +3,13 @@
 namespace App\Http\Livewire\Admin;
 
 use App\Models;
-use App\Models\Registration;
-use App\Models\Section;
-use App\Models\Subject;
-use App\Models\Status;
-use App\Models\User;
 use Carbon\Carbon;
 use Livewire\Component;
 
 class Dashboard extends Component
 {
+    public $userCollection;
+    public $registrationCollection;
     public $programs, $programsData;
 
     public function mount()
@@ -21,40 +18,74 @@ class Dashboard extends Component
 
         $programIds = $this->programs->pluck('id')->toArray();
 
+        $students = Models\Student::all();
+
         foreach ($programIds as $id) {
-            $this->programsData[] = Models\Student::filterByProgram($id)->count();
+            $this->programsData[] = $students->filter( function ($student) use ($id) {
+                return $student->program_id == $id;
+            })->count();
         }
+
+        $this->registrationCollection = Models\Registration::get();
+
+        $this->userCollection = Models\User::with(['role', 'person.detail'])->get();
     }
 
     public function render()
     {
         return view('livewire.admin.dashboard', [
-            'users' => Models\User::get('id')->count(),
-            'registrations' => Models\Registration::get('id')->count(),
+            'users' => $this->userCollection->count(),
+            'registrations' => $this->registrationCollection->count(),
             'sections' => Models\Section::get('id')->count(),
             'subjects' => Models\Subject::get('id')->count(),
-            'female' => Models\User::female()->count(),
-            'male' => Models\User::male()->count(),
-            'other' => Models\User::other()->count(),
-            'prefer' => Models\User::none()->count(),
-            'admin' => Models\User::get(['id', 'role_id'])->where('role_id', 1)->count(),
-            'student' => Models\User::get(['id', 'role_id'])->where('role_id', 2)->count(),
-            'registrar' => Models\User::get(['id', 'role_id'])->where('role_id', 3)->count(),
-            'dean' => Models\User::get(['id', 'role_id'])->where('role_id', 4)->count(),
-            'faculty' => Models\User::get(['id', 'role_id'])->where('role_id', 5)->count(),
-            'enrolled' => Models\Registration::enrolled()->get()->count(),
-            'finalized' => Models\Registration::finalized()->count(),
-            'confirming' => Models\Registration::confirming()->count(),
-            'pending' => Models\Registration::pending()->count(),
+            'female' => $this->userCollection->filter(function ($user) {
+                return $user->person->detail->gender == 'Female';
+            })->count(),
+            'male' => $this->userCollection->filter(function ($user) {
+                return $user->person->detail->gender == 'Male';
+            })->count(),
+            'other' => $this->userCollection->filter(function ($user) {
+                return $user->person->detail->gender == 'Other';
+            })->count(),
+            'prefer' => $this->userCollection->filter(function ($user) {
+                return $user->person->detail->gender == 'Prefer not to say';
+            })->count(),
+            'admin' => $this->userCollection->filter(function ($user) {
+                return $user->role->name == 'admin';
+            })->count(),
+            'student' => $this->userCollection->filter(function ($user) {
+                return $user->role->name == 'student';
+            })->count(),
+            'registrar' => $this->userCollection->filter(function ($user) {
+                return $user->role->name == 'registrar';
+            })->count(),
+            'dean' => $this->userCollection->filter(function ($user) {
+                return $user->role->name == 'dean';
+            })->count(),
+            'faculty' => $this->userCollection->filter(function ($user) {
+                return $user->role->name == 'faculty member';
+            })->count(),
+            'enrolled' => $this->registrationCollection->filter(function ($registration) {
+                return $registration->status_id == 4;
+            })->count(),
+            'finalized' => $this->registrationCollection->filter(function ($registration) {
+                return $registration->status_id == 3;
+            })->count(),
+            'confirming' => $this->registrationCollection->filter(function ($registration) {
+                return $registration->status_id == 2;
+            })->count(),
+            'pending' => $this->registrationCollection->filter(function ($registration) {
+                return $registration->status_id == 1;
+            })->count(),
             'programsCode' => $this->programs->pluck('code')->toArray(),
             'programsTableTitle' => "Number of students per program as of SY. ".Carbon::parse(now())->format('Y').'-'.Carbon::parse(now())->addYear()->format('Y'),
         ]);
     }
 
     public function getRecentlyEnrolleesProperty() {
-        $status = Status::where('name', 'enrolled')->firstOrFail();
+        $status = Models\Status::where('name', 'enrolled')->firstOrFail();
 
-        return Registration::with('student.user.person')
+        return Models\Registration::with('student.user.person')
             ->where('status_id', $status->id)
             ->latest()
             ->take(5)
--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -1,78 +1,203 @@
-<!-- Responsive Navigation Menu -->
-<div @click="open = ! open" :class="{'w-full': open, 'lg:w-12': ! open}" class="pb-14 w-0 h-full bg-gray-500 bg-opacity-50 fixed">
+<div @click="open = ! open"
+     :class="{'w-full': open, 'lg:w-12': ! open}"
+     class="pb-14 w-0 h-full bg-gray-500 bg-opacity-50 fixed">
 
-    <div @click.stop :class="{'w-full sm:w-1/2 lg:w-64 shadow-lg': open, 'w-0 lg:w-12': ! open}" class="sidebar overflow-y-auto overflow-x-hidden transition-width transition-slowest ease h-full bg-white border-r border-gray-200">
+    <div @click.stop
+         :class="{'w-full sm:w-1/2 lg:w-64 shadow-lg': open, 'w-0 lg:w-12': ! open}"
+         class="sidebar overflow-y-auto overflow-x-hidden transition-width transition-slowest ease h-full bg-white border-r border-gray-200">
+
         <ul class="flex flex-col h-content w-full">
             @if (auth()->user()->role->name === 'admin')
                 <x-sidebar.item title="Dashboard">
-                    <x-sidebar.link routeName="admin.dashboard" route="admin/dashboard" name="Dashboard">
+                    <x-sidebar.link routeName="admin.dashboard" :routes="['admin.dashboard']" name="Dashboard">
                         <x-icons.dashboard-icon/>
                     </x-sidebar.link>
                 </x-sidebar.item>
 
-                <x-sidebar.item title="Pre Enrollment">
-                    <x-sidebar.link routeName="admin.pre.enrollments.view" route="pre-enrollments" name="Pre Enrollment">
+                <div x-data="{ dropdown: false }">
+                    <x-sidebar.dropdown :routes="['admin.pre.enrollments.view', 'admin.advising.view', 'sections.view']" title="Pre Enrollment">
                         <x-icons.pre-enrollment-icon/>
-                    </x-sidebar.link>
-                </x-sidebar.item>
+                    </x-sidebar.dropdown>
 
-                <x-sidebar.item title="Sections">
-                    <x-sidebar.link routeName="sections.view" route="sections" name="Sections">
-                        <x-icons.section-icon/>
-                    </x-sidebar.link>
-                </x-sidebar.item>
+                    <x-sidebar.item
+                        x-show="dropdown"
+                        class="bg-gray-200"
+                        title="View List - Pre Enrollments">
 
-                <x-sidebar.item title="Faculties">
-                    <x-sidebar.link routeName="admin.faculties.view" route="admin/faculties" name="Faculties">
-                        <x-icons.faculty-icon/>
-                    </x-sidebar.link>
-                </x-sidebar.item>
+                        <x-sidebar.link
+                            routeName="admin.pre.enrollments.view"
+                            :routes="['admin.pre.enrollments.view']"
+                            name="View List"
+                            activeColor="bg-indigo-100 hover:bg-indigo-50"
+                            defaultColor="bg-gray-100 hover:bg-gray-200">
+                            <span class="text-black"><x-icons.list-icon stroke-width="2.5"/></span>
+                        </x-sidebar.link>
+                    </x-sidebar.item>
 
-                <x-sidebar.item title="Grades">
-                    <x-sidebar.link routeName="admin.grades.view" route="grades" name="Grades">
+                    <x-sidebar.item
+                        x-show="dropdown"
+                        class="bg-gray-200"
+                        title="Advising Schedules">
+
+                        <x-sidebar.link
+                            routeName="admin.advising.view"
+                            :routes="['admin.advising.view']"
+                            name="Advising Schedules"
+                            activeColor="bg-indigo-100 hover:bg-indigo-50"
+                            defaultColor="bg-gray-100 hover:bg-gray-200">
+                            <x-icons.advising-icon/>
+                        </x-sidebar.link>
+                    </x-sidebar.item>
+
+                    <x-sidebar.item
+                        x-show="dropdown"
+                        class="bg-gray-200"
+                        title="Sections">
+
+                        <x-sidebar.link
+                            routeName="sections.view"
+                            :routes="['sections.view']"
+                            name="Sections"
+                            activeColor="bg-indigo-100 hover:bg-indigo-50"
+                            defaultColor="bg-gray-100 hover:bg-gray-200">
+                            <x-icons.section-icon/>
+                        </x-sidebar.link>
+                    </x-sidebar.item>
+                </div>
+
+                <div x-data="{ dropdown: false }">
+                    <x-sidebar.dropdown :routes="['admin.curricula.view', 'admin.prospectuses.view', 'admin.programs.view', 'admin.subjects.view']" title="Curricula">
+                        <x-icons.curriculum-icon/>
+                    </x-sidebar.dropdown>
+
+                    <x-sidebar.item
+                        x-show="dropdown"
+                        class="bg-gray-200"
+                        title="View List - Curricula">
+
+                        <x-sidebar.link
+                            routeName="admin.curricula.view"
+                            :routes="['admin.curricula.view']"
+                            name="View List"
+                            activeColor="bg-indigo-100 hover:bg-indigo-50"
+                            defaultColor="bg-gray-100 hover:bg-gray-200">
+                            <span class="text-black"><x-icons.list-icon stroke-width="2.5"/></span>
+                        </x-sidebar.link>
+                    </x-sidebar.item>
+
+                    <x-sidebar.item
+                        x-show="dropdown"
+                        class="bg-gray-200"
+                        title="Prospectus">
+
+                        <x-sidebar.link
+                            routeName="admin.prospectuses.view"
+                            :routes="['admin.prospectuses.view']"
+                            name="Prospectus"
+                            activeColor="bg-indigo-100 hover:bg-indigo-50"
+                            defaultColor="bg-gray-100 hover:bg-gray-200">
+                            <x-icons.prospectus-icon/>
+                        </x-sidebar.link>
+                    </x-sidebar.item>
+
+                    <x-sidebar.item
+                        x-show="dropdown"
+                        class="bg-gray-200"
+                        title="Programs">
+
+                        <x-sidebar.link
+                            routeName="admin.programs.view"
+                            :routes="['admin.programs.view']"
+                            name="Programs"
+                            activeColor="bg-indigo-100 hover:bg-indigo-50"
+                            defaultColor="bg-gray-100 hover:bg-gray-200">
+                            <x-icons.program-icon/>
+                        </x-sidebar.link>
+                    </x-sidebar.item>
+
+                    <x-sidebar.item
+                        x-show="dropdown"
+                        class="bg-gray-200"
+                        title="Subjects">
+
+                        <x-sidebar.link
+                            routeName="admin.subjects.view"
+                            :routes="['admin.subjects.view']"
+                            name="Subjects"
+                            activeColor="bg-indigo-100 hover:bg-indigo-50"
+                            defaultColor="bg-gray-100 hover:bg-gray-200">
+                            <x-icons.subject-icon/>
+                        </x-sidebar.link>
+                    </x-sidebar.item>
+                </div>
+
+                <x-sidebar.item title="Grading">
+                    <x-sidebar.link routeName="admin.grades.view" :routes="['admin.grades.view']" name="Grading">
                         <x-icons.grade-icon/>
                     </x-sidebar.link>
                 </x-sidebar.item>
 
-                <x-sidebar.item title="Prospectus">
-                    <x-sidebar.link routeName="admin.prospectuses.view" route="admin/prospectuses" name="Prospectus">
-                        <x-icons.prospectus-icon/>
-                    </x-sidebar.link>
-                </x-sidebar.item>
-
-                <x-sidebar.item title="Subjects">
-                    <x-sidebar.link routeName="admin.subjects.view" route="admin/subjects" name="Subjects">
-                        <x-icons.subject-icon/>
-                    </x-sidebar.link>
-                </x-sidebar.item>
-
-                <x-sidebar.item title="Programs">
-                    <x-sidebar.link routeName="admin.programs.view" route="admin/programs" name="Programs">
-                        <x-icons.program-icon/>
+                <x-sidebar.item title="Faculties">
+                    <x-sidebar.link routeName="admin.faculties.view" :routes="['admin.faculties.view']" name="Faculties">
+                        <x-icons.faculty-icon/>
                     </x-sidebar.link>
                 </x-sidebar.item>
 
                 <x-sidebar.item title="Fees">
-                    <x-sidebar.link routeName="admin.fees.view" route="admin/fees" name="Fees">
+                    <x-sidebar.link routeName="admin.fees.view" :routes="['admin.fees.view']" name="Fees">
                         <x-icons.fee-icon/>
                     </x-sidebar.link>
                 </x-sidebar.item>
 
                 <x-sidebar.item title="Payments">
-                    <x-sidebar.link routeName="admin.payments.view" route="admin/payments" name="Payments">
+                    <x-sidebar.link routeName="admin.payments.view" :routes="['admin.payments.view']" name="Payments">
                         <x-icons.credit-card-icon/>
                     </x-sidebar.link>
                 </x-sidebar.item>
 
-                <x-sidebar.item title="Advising">
-                    <x-sidebar.link routeName="admin.advising.view" route="admin/advising" name="Advising">
-                        <x-icons.advising-icon/>
-                    </x-sidebar.link>
-                </x-sidebar.item>
-
-                <x-sidebar.item title="Users">
-                    <x-sidebar.link routeName="admin.users.view" route="admin/users" name="Users">
+                <div x-data="{ dropdown: false }">
+                    <x-sidebar.dropdown :routes="['admin.users.view', 'users.students.index']" title="Users">
                         <x-icons.users-icon/>
+                    </x-sidebar.dropdown>
+
+                    <x-sidebar.item
+                        x-show="dropdown"
+                        class="bg-gray-200"
+                        title="View List - All Users">
+
+                        <x-sidebar.link
+                            routeName="admin.users.view"
+                            :routes="['admin.users.view']"
+                            name="View List"
+                            activeColor="bg-indigo-100 hover:bg-indigo-50"
+                            defaultColor="bg-gray-100 hover:bg-gray-200">
+                            <span class="text-black"><x-icons.list-icon stroke-width="2.5"/></span>
+                        </x-sidebar.link>
+                    </x-sidebar.item>
+
+                    <x-sidebar.item
+                        x-show="dropdown"
+                        class="bg-gray-200"
+                        title="Students">
+
+                        <x-sidebar.link
+                            routeName="users.students.index"
+                            :routes="['users.students.index']"
+                            name="Students"
+                            activeColor="bg-indigo-100 hover:bg-indigo-50"
+                            defaultColor="bg-gray-100 hover:bg-gray-200">
+                            <span class="text-black"><x-icons.student-icon stroke-width="2.5"/></span>
+                        </x-sidebar.link>
+                    </x-sidebar.item>
+                </div>
+
+                <x-sidebar.item title="Settings">
+                    <x-sidebar.link routeName="admin.settings" :routes="['admin.settings']" name="Settings">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                            <path d="M10.325 4.317c.426 -1.756 2.924 -1.756 3.35 0a1.724 1.724 0 0 0 2.573 1.066c1.543 -.94 3.31 .826 2.37 2.37a1.724 1.724 0 0 0 1.065 2.572c1.756 .426 1.756 2.924 0 3.35a1.724 1.724 0 0 0 -1.066 2.573c.94 1.543 -.826 3.31 -2.37 2.37a1.724 1.724 0 0 0 -2.572 1.065c-.426 1.756 -2.924 1.756 -3.35 0a1.724 1.724 0 0 0 -2.573 -1.066c-1.543 .94 -3.31 -.826 -2.37 -2.37a1.724 1.724 0 0 0 -1.065 -2.572c-1.756 -.426 -1.756 -2.924 0 -3.35a1.724 1.724 0 0 0 1.066 -2.573c-.94 -1.543 .826 -3.31 2.37 -2.37c1 .608 2.296 .07 2.572 -1.065z"></path>
+                            <circle cx="12" cy="12" r="3"></circle>
+                        </svg>
                     </x-sidebar.link>
                 </x-sidebar.item>
             @elseif (auth()->user()->role->name === 'registrar')
