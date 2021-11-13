c6c3984382c994e654edeff83f3e119cd84b89bd
jadessoriano <jadelestersoriano@gmail.com>
2021-11-13 18:04:59 +0800

    update: setting seeder

--- database/seeders/SettingSeeder.php
+++ database/seeders/SettingSeeder.php
@@ -17,8 +17,10 @@ class SettingSeeder extends Seeder
         DB::table('settings')->delete();
 
         $setting = [
+            'profile_photo_path' => 'https://res.cloudinary.com/ds2znjplk/image/upload/v1636794692/ue5w8r6b4rneytnblmnk.svg',
             'school_name' => 'University',
             'school_email' => 'olcollegeenrollmentsystem@gmail.com',
+            'school_address' => 'Alcalde St., Kapisagan Pasig City',
             'auto_account_approval' => 1,
             'allow_irregular_student_to_enroll' => 1,
             'created_at' => now(),
