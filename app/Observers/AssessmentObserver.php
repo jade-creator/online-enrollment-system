c89ec46092a747b052caa709559c11560d8963d7
jadessoriano <jadelestersoriano@gmail.com>
2021-12-13 02:51:43 +0800

    automatic enrollment for unifast scholarship

--- app/Observers/AssessmentObserver.php
+++ app/Observers/AssessmentObserver.php
@@ -3,11 +3,20 @@
 namespace App\Observers;
 
 use App\Models\Assessment;
+use App\Services\Registration\RegistrationStatusService;
 
 class AssessmentObserver
 {
     public function creating(Assessment $assessment)
     {
         $assessment->approver_id = auth()->user()->id;
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public function created(Assessment $assessment)
+    {
+        if ($assessment->isUnifastBeneficiary) (new RegistrationStatusService())->enroll($assessment->registration);
     }
 }
